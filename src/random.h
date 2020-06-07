4705be1117645e7effd695f3ae2aef0a23fb3e03
LWY <668915+laxa88@users.noreply.github.com>
2020-06-07 14:28:28 +0900

    update random.h

--- src/random.h
+++ src/random.h
@@ -4,11 +4,15 @@
 #include <emscripten.h>
 #endif
 
-unsigned char random(int mod)
+// Returns a Uint16 number from 0 to mod.
+Uint16 random(int mod)
 {
 #ifdef __EMSCRIPTEN__
+    // Javascript returns a value from 0 to 1.
     return emscripten_random() * mod;
 #else
+    // http://www.cplusplus.com/reference/cstdlib/RAND_MAX/
+    // A number between 0 to RAND_MAX. Guarantees at least 32767.
     return rand() % mod;
 #endif
 }
