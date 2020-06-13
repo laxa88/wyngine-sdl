2ff1ffd238e43a6c90ddcb7b93f67a5d6a8cdaac
LWY <668915+laxa88@users.noreply.github.com>
2020-06-13 12:25:40 +0900

    new visualizer demo

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@
 # emcc main2.cpp -O3 --closure 1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS=["png"] --preload-file assets -o bin-js/webgame.html
 
 all : \
-	audio-demo
+	audio-visualizer-demo
 
 keyboard-demo:
 	g++ -g keyboard-demo.cpp \
@@ -47,3 +47,16 @@ audio-demo:
 	-lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lsetupapi -lversion \
 	-static \
 	-o bin\audio-demo
+
+audio-visualizer-demo:
+	g++ -g audio-visualizer-demo.cpp \
+	-IC:\wy-dev\sdl2-mingw-32\include \
+	-IC:\wy-dev\sdl2-mingw-32\include\SDL2 \
+	-IC:\wy-dev\SDL2_image-2.0.5\i686-w64-mingw32\include \
+	-LC:\wy-dev\sdl2-mingw-32\lib \
+	-LC:\wy-dev\sdl2-mingw-32\lib\SDL2 \
+	-LC:\wy-dev\SDL2_image-2.0.5\i686-w64-mingw32\lib \
+	-lmingw32 -lSDL2main -lSDL2 -lSDL2_image  \
+	-lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lsetupapi -lversion \
+	-static \
+	-o bin\audio-visualizer-demo
---
+++ audio-visualizer-demo.cpp
@@ -0,0 +1,210 @@
+#include "src/wyngine.h"
+#include "src/font.h"
+#include "src/audio.h"
+
+class GameAudio : public WY_Audio
+{
+public:
+    WY_OscillatorType mInstrument = OSC_SINE;
+
+    GameAudio() : WY_Audio(44100, 1024, 1, 1000) {}
+
+    void setInstrument(WY_OscillatorType nType)
+    {
+        mInstrument = nType;
+    }
+
+    double getAudioSample(double dTime)
+    {
+        return WY_Oscillator::oscillate(getNote(), dTime, mInstrument);
+    }
+};
+
+class Game : public Wyngine
+{
+    int frame;
+    WY_Image *mFontImage;
+    WY_MonoFont *mFont;
+    GameAudio *audio;
+
+    void loadMedia()
+    {
+        mFontImage = loadPNG(mRenderer, "assets/ascii-bnw.png");
+    }
+
+public:
+    Game() : Wyngine("Wyngine audio demo", 256, 224, 2)
+    {
+        loadMedia();
+
+        mFont = new WY_MonoFont(mFontImage->texture, 8, 4, {8, 8, 240, 208});
+        mFont->setDebug(true);
+
+        audio = new GameAudio();
+    }
+
+    ~Game()
+    {
+        SDL_DestroyTexture(mFontImage->texture);
+        delete mFontImage;
+
+        delete mFont;
+
+        delete audio;
+    }
+
+    void onUpdate()
+    {
+        // audio settings
+
+        if (keyboard->isKeyPressed(SDLK_q))
+        {
+            audio->mSampleRate += 1000;
+        }
+        else if (keyboard->isKeyPressed(SDLK_a))
+        {
+            audio->mSampleRate -= 1000;
+        }
+        // else if (keyboard->isKeyPressed(SDLK_w))
+        // {
+        //     audio->mSampleSize *= 2;
+        // }
+        // else if (keyboard->isKeyPressed(SDLK_s))
+        // {
+        //     audio->mSampleSize /= 2;
+        // }
+        else if (keyboard->isKeyPressed(SDLK_e))
+        {
+            audio->mAmplitude += 100;
+        }
+        else if (keyboard->isKeyPressed(SDLK_d))
+        {
+            audio->mAmplitude -= 100;
+        }
+
+        // instrument settings
+
+        if (keyboard->isKeyPressed(SDLK_1))
+        {
+            audio->setInstrument(OSC_SINE);
+        }
+        else if (keyboard->isKeyPressed(SDLK_2))
+        {
+            audio->setInstrument(OSC_SQUARE);
+        }
+        else if (keyboard->isKeyPressed(SDLK_3))
+        {
+            audio->setInstrument(OSC_TRIANGLE);
+        }
+        else if (keyboard->isKeyPressed(SDLK_4))
+        {
+            audio->setInstrument(OSC_SAW_ANALOGUE);
+        }
+        else if (keyboard->isKeyPressed(SDLK_5))
+        {
+            audio->setInstrument(OSC_SAW_OPTIMIZED);
+        }
+        else if (keyboard->isKeyPressed(SDLK_6))
+        {
+            audio->setInstrument(OSC_NOISE);
+        }
+
+        // music octave
+
+        if (keyboard->isKeyPressed(SDLK_UP))
+        {
+            audio->increaseOctave();
+        }
+        else if (keyboard->isKeyPressed(SDLK_DOWN))
+        {
+            audio->decreaseOctave();
+        }
+
+        // music notes
+
+        if (keyboard->isKeyDown(SDLK_z))
+        {
+            audio->speak(NOTE_A);
+        }
+        else if (keyboard->isKeyDown(SDLK_s))
+        {
+            audio->speak(NOTE_AS);
+        }
+        else if (keyboard->isKeyDown(SDLK_x))
+        {
+            audio->speak(NOTE_B);
+        }
+        else if (keyboard->isKeyDown(SDLK_c))
+        {
+            audio->speak(NOTE_C);
+        }
+        else if (keyboard->isKeyDown(SDLK_f))
+        {
+            audio->speak(NOTE_CS);
+        }
+        else if (keyboard->isKeyDown(SDLK_v))
+        {
+            audio->speak(NOTE_D);
+        }
+        else if (keyboard->isKeyDown(SDLK_g))
+        {
+            audio->speak(NOTE_DS);
+        }
+        else if (keyboard->isKeyDown(SDLK_b))
+        {
+            audio->speak(NOTE_E);
+        }
+        else if (keyboard->isKeyDown(SDLK_n))
+        {
+            audio->speak(NOTE_F);
+        }
+        else if (keyboard->isKeyDown(SDLK_j))
+        {
+            audio->speak(NOTE_FS);
+        }
+        else if (keyboard->isKeyDown(SDLK_m))
+        {
+            audio->speak(NOTE_G);
+        }
+        else if (keyboard->isKeyDown(SDLK_k))
+        {
+            audio->speak(NOTE_GS);
+        }
+        else
+        {
+            audio->silence();
+        }
+    }
+
+    void onRender()
+    {
+        std::string t1 = "Time elapsed        : ";
+        std::string t2 = std::to_string(timer->getTimeSinceStart());
+        std::string t3 = "\n\nfrequency / current sample :\n";
+        std::string t4 = std::to_string(audio->mSampleRate);
+        std::string t4a = " / ";
+        std::string t4b = std::to_string(audio->mSampleIndex);
+        std::string t5 = "\n\nSample size         : ";
+        std::string t6 = std::to_string(audio->mSampleSize);
+        std::string t7 = "\nAmplitude (volume)  : ";
+        std::string t8 = std::to_string(audio->mAmplitude);
+        std::string t9 = "\nChannels (speakers) : ";
+        std::string t10 = std::to_string(audio->mChannels);
+
+        std::string s1 = "\n\nInstrument : ";
+        std::string s2 = WY_Oscillator::getOscillatorTypeName(audio->mInstrument);
+        std::string s3 = "\nOctave     : ";
+        std::string s4 = std::to_string(audio->mOctave);
+
+        mFont->print(mRenderer, t1 + t2 + t3 + t4 + t4a + t4b + t5 + t6 + t7 + t8 + t9 + t10 + s1 + s2 + s3 + s4);
+    }
+};
+
+int main(int argc, char *args[])
+{
+    Game *game = new Game();
+
+    game->run();
+
+    return 0;
+}
