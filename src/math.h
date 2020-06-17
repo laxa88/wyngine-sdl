ac005e31e269f17b9a51bff69da7ba22ab7d39ba
LWY <668915+laxa88@users.noreply.github.com>
2020-06-17 20:08:43 +0900

    refactor wyrandom to template

--- src/math.h
+++ src/math.h
@@ -9,15 +9,16 @@
 #define PI M_PI
 #define TWO_PI (2.0 * M_PI)
 
-// Returns a Uint16 number from 0 to mod.
-Uint16 wyrandom(int mod)
+// Returns a number from 0 to mod.
+template <class T = Uint16>
+T wyrandom(int mod)
 {
 #ifdef __EMSCRIPTEN__
     // Javascript returns a value from 0 to 1.
-    return emscripten_random() * mod;
+    return (T)(emscripten_random() * mod);
 #else
     // http://www.cplusplus.com/reference/cstdlib/RAND_MAX/
     // A number between 0 to RAND_MAX. Guarantees at least 32767.
-    return rand() % mod;
+    return (T)(rand() % mod);
 #endif
 }
